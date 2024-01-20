7294e043e3e0004bb93fcfd1410e13de7f6b556f
Dhiraj <iamdhirajkrprasad@gmail.com>
2024-01-20 19:49:00 +0530

    mulit value return functions

--- firstProject/lib/functions.go
+++ firstProject/lib/functions.go
@@ -5,7 +5,7 @@ func Concat(s1 string, s2 string) string {
 }
 
 // we can use the comma separated params if they are of the same type an just declare the type once.
-func add(a, b int) int {
+func Add(a, b int) int {
 	return a + b
 }
 
@@ -17,3 +17,21 @@ func GetFullName(firstname string, lastnname string) (fullname string) {
 }
 
 // in the above function we have predifned the retun variable along with the return type.
+
+/*-----------------funcitons as varibles----------------*/
+
+// functions are treated as variables in go, i.e they are first class citizens, thus we can pass and return a fuction to and from and another fucntion.
+
+//lets take a fucntion example which takes a  function as its argument.
+
+func AddThree(addTwo func(int, int) int, c int) int {
+	return addTwo(2, 3) + c
+}
+
+// we can also return more than one value from a function , this patter is common where the value and the error is returned from a function.
+
+func Divide(a, b int) (coef, rem int) {
+	coef = a / b
+	rem = a % b
+	return
+}
--- firstProject/main.go
+++ firstProject/main.go
@@ -7,4 +7,9 @@ import (
 
 func main(){
 	fmt.Println(lib.Concat("dhiraj" , "Prasad"))
+	a , b := 10,3
+	coef ,rem := lib.Divide(a,b)
+
+
+	fmt.Printf("The coeffecient and reamainder of %v / %v is : %v , %v ", a, b, coef, rem);
 }
